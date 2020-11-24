a7e6869406be81f43e82b0b71c9db96a38fc15a2
Dave <HSC.Coach@gmail.com>
2020-11-24 16:32:50 -0500

    babysitter first test passed

--- src/main/java/babysitter/WageCalculator.java
+++ src/main/java/babysitter/WageCalculator.java
@@ -2,8 +2,14 @@ package babysitter;
 
 public class WageCalculator {
     public int calculateShiftWage(int startingHour, int endingHour, int bedtime) {
-
+        if (startingHour <= 4) {
+            startingHour += 24;
+        }
+        if (startingHour < bedtime) {
+            return 12;
+        }
         return 0;
     }
-
 }
+
+
