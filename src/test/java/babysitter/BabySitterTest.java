f9825097369770d29deaf0db4361d5f128f04f05
David Alvarado <HSC.Coach@gmail.com>
2020-11-25 16:07:32 -0500

    first babysitter test refactor

--- src/test/java/babysitter/BabySitterTest.java
+++ src/test/java/babysitter/BabySitterTest.java
@@ -1,35 +1,40 @@
 package babysitter;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class BabySitterTest {
+
+    private WageCalculator underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new WageCalculator();
+    }
+
     @Test
     public void oneHourPreBedTimeShouldPay12() {
-        WageCalculator underTest = new WageCalculator();
         int wage = underTest.calculateShiftWage(17, 18, 20);
         assertThat(wage).isEqualTo(12);
     }
 
     @Test
     public void oneHourBeforeMidnightAfterBedtimeShouldPay8(){
-        WageCalculator underTest = new WageCalculator();
         int wage = underTest.calculateShiftWage(22, 23, 21);
         assertThat(wage).isEqualTo(8);
     }
 
     @Test
     public void oneHourAfterMidnightShouldPay16(){
-        WageCalculator underTest = new WageCalculator();
         int wage = underTest.calculateShiftWage(24, 25, 21);
         assertThat(wage).isEqualTo(16);
     }
 
     @Test
     public void entireShiftShouldPay132(){
-        WageCalculator underTest = new WageCalculator();
         int wage = underTest.calculateShiftWage(17, 4, 20);
         assertThat(wage).isEqualTo(132);
     }
